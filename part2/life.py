be4245cf49f9ad23121acb7ad5567dab5daf8fc8
turnergarrow <tugarrow15@gmail.com>
2019-03-29 13:45:35 -0400

    Calculates number of neighbours

--- part2/life.py
+++ part2/life.py
@@ -7,10 +7,6 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 def readFile(filename):
-    """
-        Reads the grids from a text file.
-        Saves them as 2D arrays
-    """
     with open(filename) as f:
         file = f.readlines()
     board = []
@@ -30,9 +26,33 @@ def animate(vals):
     ani = FuncAnimation(fig, update_plot, frames=len(vals), interval=1000)
     plt.show()
 
-def step(board):
-    pass
+def num_neighbours(x, y):
+    total = 0
+    if y > 0:
+        total += board[y-1][x]
+    if y < len(board[0])-1:
+        total += board[y+1][x]
+    if x > 0:
+        total += board[y][x-1]
+        if y > 0:
+            total += board[y-1][x-1]
+        if y < len(board)-1:
+            total += board[y+1][x-1]
+    if x < len(board[0])-1:
+        total += board[y][x+1]
+        if y > 0:
+            total += board[y-1][x+1]
+        if y < len(board[0])-1:
+            total += board[y+1][x+1]
+    return total
+
+def step():
+    next = [[0 for i in range(len(board[0]))] for j in range(len(board))]
+    for y in range(len(board)):
+        for x in range(len(board[0])):
+            print(x, y, num_neighbours(x, y))
 
 n, board = readFile("inLife.txt")
-vals = [board]
+vals = [board, np.transpose(board)]
+step()
 animate(vals)
