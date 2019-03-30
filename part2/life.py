322ecdb16333110a48870434d122fdf469974c84
turnergarrow <tugarrow15@gmail.com>
2019-03-30 11:08:18 -0400

    Fixed issue with calculating neighbours

--- part2/life.py
+++ part2/life.py
@@ -12,11 +12,19 @@ def readFile(filename):
     with open(filename) as f:
         file = f.readlines()
     board = []
-    nsteps = str(file[0])
+    nsteps = int(file[0])
     for row in file[1:]:
 
         board.append([int(x) for x in list(row.rstrip('\n'))])
     return nsteps, board
+
+def writeOutput(frames, filename):
+    with open(filename,'w') as out:
+        for ind in range(len(frames)):
+            out.write('Generation'+str(ind)+"\n")
+            for row in frames[ind]:
+                line = ''.join(str(x) for x in row)+"\n"
+                out.write(line)
 
 def animate(vals):
     fig, axes = plt.subplots()
@@ -24,35 +32,37 @@ def animate(vals):
         print(i)
         axes.clear()
         im = axes.pcolormesh(vals[i], cmap="Greys", vmin=0, vmax=1)
+        ax = plt.gca()
+        ax.invert_yaxis()
         return axes
     ani = FuncAnimation(fig, update_plot, frames=len(vals), interval=500)
     plt.show()
 
-def num_neighbours(x, y):
+def num_neighbours(x, y, board):
     total = 0
     if y > 0:
-        total += board[y-1][x]
+        total += board[y-1][x] # up
     if y < len(board[0])-1:
-        total += board[y+1][x]
+        total += board[y+1][x] # down
     if x > 0:
-        total += board[y][x-1]
+        total += board[y][x-1] # left
         if y > 0:
-            total += board[y-1][x-1]
+            total += board[y-1][x-1] # up-left diagonal
         if y < len(board)-1:
-            total += board[y+1][x-1]
+            total += board[y+1][x-1] # down-left diagonal
     if x < len(board[0])-1:
-        total += board[y][x+1]
+        total += board[y][x+1] # right
         if y > 0:
-            total += board[y-1][x+1]
+            total += board[y-1][x+1] # up-right
         if y < len(board[0])-1:
-            total += board[y+1][x+1]
+            total += board[y+1][x+1] # down-right
     return total
 
 def step(board):
     next = deepcopy(board)
     for y in range(len(board)):
         for x in range(len(board[0])):
-            n = num_neighbours(x, y)
+            n = num_neighbours(x, y, board)
             alive = board[y][x]
             if alive and n < 2:
                 next[y][x] = 0 # Dies :(
@@ -65,7 +75,10 @@ def step(board):
     return next
 
 n, board = readFile("inLife.txt")
-next = step(board)
-#nnext = step(next)
-vals = [board, next]
-animate(vals)
+next = board
+frames = [board]
+for i in range(n):
+    next = step(next)
+    frames.append(next)
+writeOutput(frames, "outLife.txt")
+animate(frames)
