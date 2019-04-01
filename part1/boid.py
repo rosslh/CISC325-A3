14b5a39420b06a3042f4e0fa1ad0821ec0498e58
Ross Hill <rosshill@protonmail.com>
2019-04-01 19:30:05 -0400

    Limit boid speed Add wind simulation Add moveTo function

--- part1/boid.py
+++ part1/boid.py
@@ -2,21 +2,48 @@ import math
 
 
 class Boid:
-    def __init__(self, id, velocity, position, boundary):
+    def __init__(self, id, velocity, position, boundary, size=0):
         self.id = id
+        self.size = size
         self.boundary = boundary
         self.position = position  # x, y
         self.velocity = velocity  # x, y
 
-    def update_position(self, boids):
+    def update_position(self, boids, windVector):
         self.rule1(boids)
         self.rule2(boids)
         self.rule3(boids)
+
+        self.applyWind(windVector)
+
+        self._limitVelocity()
+
         self.position[0] += self.velocity[0]
         self.position[1] += self.velocity[1]
 
         self.position[0] %= self.boundary
         self.position[1] %= self.boundary
+
+    '''
+        Use the sigmoid function to taper off velocity if it is accelerating out 
+        of control due to the emergent behaviour of the system
+    '''
+
+    def applyWind(self, windVector):
+        print(windVector)
+        self.velocity[0] += windVector[0]
+        self.velocity[1] += windVector[1]
+
+    def _limitVelocity(self):
+        velocityRange = 150  # max value is velocity range/2,
+        self.velocity[0] = self._sigmoid(self.velocity[0], velocityRange)
+        self.velocity[1] = self._sigmoid(self.velocity[1], velocityRange)
+
+    def _sigmoid(self, x, velocityRange):
+        div5 = velocityRange/5
+        denominator = 1 + math.exp(-(1/div5) * x)
+        fraction = (1/denominator)*velocityRange
+        return fraction-(velocityRange/2)
 
     # moves boid to middle of center of mass
     def rule1(self, boids):
@@ -45,7 +72,6 @@ class Boid:
                     vector[1] -= self.position[1]-boid.position[1]
         self.velocity[0] += vector[0]
         self.velocity[1] += vector[1]
-        print(vector, self.velocity)
 
     # Align boid velocities
 
@@ -59,14 +85,15 @@ class Boid:
         flockVelocity[0] /= len(boids) - 1 or 1
         flockVelocity[1] /= len(boids) - 1 or 1
         self.velocity[0] += (flockVelocity[0] -
-                             self.position[0]) / 8  # weighted 1/8
-        self.velocity[1] += (flockVelocity[1] - self.position[1]) / 8
+                             self.velocity[0]) / 8  # weighted 1/8
+        self.velocity[1] += (flockVelocity[1] - self.velocity[1]) / 8
 
     def _inSameNeighborhood(self, boid):
-        return self._distance(boid) < 30
+        return self._distance(boid) < 40
 
     def _distance(self, boid):
         position = boid.position
         x_dist = math.pow(self.position[0]-position[0], 2)
         y_dist = math.pow(self.position[1]-position[1], 2)
-        return math.sqrt(x_dist+y_dist)
+        # accounts for boid size
+        return max(math.sqrt(x_dist+y_dist) - self.size, 0)
--- part1/main.py
+++ part1/main.py
@@ -6,40 +6,56 @@ from tkinter import *
 import time
 import random
 
-canvasSize = 1000
+canvasSize = 800
+boidSize = 20
 
 
 def main():
-    numBoids = 1
+    numBoids = 15
     boids = []
     for i in range(numBoids):
-        boids.append(Boid(i, [0, 0], [10, 10], canvasSize))
+        boids.append(Boid(i, [random.randint(-20, 40), random.randint(-20, 40)],
+                          [random.randint(30, 90), random.randint(30, 90)], canvasSize, boidSize))
 
     gui, canvas, ovals = drawCanvas(canvasSize, boids)
-    iterations = 10
-    for i in range(iterations):
-        boidSnapshot = boids[:]
-        for boid in boids:
-            print(boid.id, boid.position, boid.velocity)
-            bId = boid.id
-            boid.update_position(boidSnapshot)
-            assert(boid.position[0] <
-                   canvasSize and boid.position[1] < canvasSize)
-            canvas.move(ovals[boid.id], boid.velocity[0], boid.velocity[1])
+    iterations = 200
+    try:
+        for i in range(iterations):
+            windVector = [sin(i / 10) * 30,
+                          0] if i > iterations / 2 else [0, 0]
+            for boid in boids:
+                print(boid.id, boid.position, boid.velocity)
+                bId = boid.id
+                boid.update_position(boids, windVector)
+                assert(boid.position[0] <
+                       canvasSize and boid.position[1] < canvasSize)
+                moveTo(canvas, ovals[boid.id],
+                       boid.position[0], boid.position[1])
+            time.sleep(.15)
             gui.update()
-            time.sleep(.1)
-        # display(boids)
+    except KeyboardInterrupt:  # close canvas in case of program quit
+        gui.destroy()
     gui.title("test")
-    gui.after(5000, gui.destroy)
+    gui.destroy()
     gui.mainloop()
+
+
+def moveTo(canvas, oval, x, y):
+    currentCoords = canvas.coords(oval)
+    currentX = (currentCoords[0] + currentCoords[2]) // 2
+    currentY = (currentCoords[1] + currentCoords[3]) // 2
+
+    xDiff = x - currentX
+    yDiff = y - currentY
+
+    canvas.move(oval, xDiff, yDiff)
 
 
 def drawCanvas(boundary, initialBoids):
     gui = Tk()
     gui.geometry("{}x{}".format(int(boundary*1.5), int(boundary*1.5)))
-    canvas = Canvas(gui, width=boundary, height=boundary)
+    canvas = Canvas(gui, width=boundary, height=boundary, background="#fafafa")
     canvas.pack()
-    boidSize = 20
     ovals = {}
     for i, boid in enumerate(initialBoids):
         oval = canvas.create_oval(
